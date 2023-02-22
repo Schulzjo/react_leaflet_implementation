e1a593d3469d92d6ba268d4f4d4eb41c4c04c289
schulzjo <schulzeeverding.johannes@gmail.com>
2023-02-22 14:47:46 +0100

    initial implementation of marker
    hardcoded yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import {MapContainer, TileLayer } from "react-leaflet";
+import {MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import './App.css';
+import {Icon} from "leaflet";
+
+type CustomMarker = {
+    position: [number, number];
+}
+
+const customIcon = new Icon({
+    iconUrl: 'https://cdn-icons-png.flaticon.com/512/443/443025.png',
+    iconSize: [38, 38],
+})
+
+const initialMarkers: CustomMarker[] = [
+    {position: [52.520645, 13.409779]},
+]
 
 function App() {
     return (
         <div className="App">
-            <MapContainer center={ [47.2154556, -1.5644531] } zoom={13}>
+            <MapContainer center={ [52.520645, 13.409779] } zoom={13}>
                 <TileLayer
                     attribution={'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'}
                     url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
                 />
+                {initialMarkers.map((marker) => (
+                    <Marker position={marker.position} icon={customIcon}>
+                    </Marker>
+                ))
+                }
             </MapContainer>
         </div>
     );
