3acbe047ef8fe0ad25949a53c5687effc1f7b11e
schulzjo <schulzeeverding.johannes@gmail.com>
2023-02-22 12:32:01 +0100

    initial implementation of MapContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import {MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <MapContainer center={ [47.2154556, -1.5644531] } zoom={13}>
+                <TileLayer
+                    attribution={'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'}
+                    url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
+                />
+            </MapContainer>
+        </div>
+    );
 }
 
 export default App;
