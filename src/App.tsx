48ae1ead62caec6d364084feca252dfca62235dd
schulzjo <schulzeeverding.johannes@gmail.com>
2023-02-22 15:31:46 +0100

    initial implementation of add marker form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {MapContainer, TileLayer, Marker } from "react-leaflet";
+import React, {useState} from 'react';
+import {MapContainer, TileLayer, Marker} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import './App.css';
 import {Icon} from "leaflet";
@@ -18,14 +18,33 @@ const initialMarkers: CustomMarker[] = [
 ]
 
 function App() {
+    let [markers, setMarkers] = useState<CustomMarker[]>(initialMarkers);
+
+    function addMarker(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const target = event.currentTarget;
+        const newMarker: CustomMarker = {
+            position: [target.lat.value as number, target.long.value as number
+            ]
+        }
+        setMarkers([...markers, newMarker])
+
+    }
+
     return (
         <div className="App">
-            <MapContainer center={ [52.520645, 13.409779] } zoom={13}>
+            <form onSubmit={addMarker}>
+                <input type="text" name="lat" placeholder="latitude"/>
+                <input type="text" name="long" placeholder="longitude"/>
+                <button>Add marker</button>
+            </form>
+            <MapContainer center={[52.520645, 13.409779]} zoom={13}>
                 <TileLayer
                     attribution={'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'}
                     url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
                 />
-                {initialMarkers.map((marker) => (
+                {markers.map((marker) => (
                     <Marker position={marker.position} icon={customIcon}>
                     </Marker>
                 ))
